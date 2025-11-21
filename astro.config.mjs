89a504996454b537d8184c92b1114f18e7dec4bb
mikan-919 <131849133+mikan-919@users.noreply.github.com>
2025-11-22 06:46:20 +0900

    feat: rootだけいったん無限テキスト完成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,39 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+
+import solidJs from '@astrojs/solid-js'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, fontProviders } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Rampart One',
+        cssVariable: '--font-family-rampart-one',
+        weights: [400],
+        display: 'swap',
+      },
+      {
+        provider: fontProviders.google(),
+        name: 'Noto Sans JP',
+        cssVariable: '--font-family-noto-sans-jp',
+        display: 'swap',
+      },
+      {
+        provider: fontProviders.google(),
+        name: 'Inter',
+        cssVariable: '--font-family-inter',
+        weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        display: 'swap',
+      },
+    ],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [solidJs()],
+})
